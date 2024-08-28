0db445204605d6622f380be61cb197bbcafe510c
Chirag Agrawal <you@example.com>
2024-08-28 19:12:08 +0530

    edit and update existing article and do some updation in form because of rails 7 syntax

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,38 @@
 class ArticlesController < ApplicationController
+  def show
+    # byebug              # it will stop the execution of server
+    @article = Article.find(params[:id])
+  end
 
-     def show
-     # byebug              # it will stop the execution of server
-      @article = Article.find(params[:id])
-     end
+  def index
+    @article = Article.all
+  end
 
-     def index
-     @article = Article.all
-     end
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    # byebug
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully entered or created.'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
