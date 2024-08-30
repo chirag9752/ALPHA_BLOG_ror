2bd17503981868b64813ef2ddec644af7f3538be
Chirag Agrawal <you@example.com>
2024-08-31 01:44:19 +0530

    add user create action i.e. signup done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # resources :articles, only: [:show , :index, :new , :create, :edit , :update, :destroy]
   resources :articles
+  get 'signup', to:'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
