e4e6023df434aa17a8a3ac14ae947b743c1c5a57
Chirag Agrawal <you@example.com>
2024-08-31 02:29:17 +0530

    add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
       @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+        flash[:notice] = "your account info successfully updated"
+        redirect_to articles_path
+          else
+            render 'edit'
+          end
+    end
+
+    # Note : Those user who have password already when we updating we don't need to enter password it's automatically updated but those user who don't
+    # have password we have to enter the new password for them when we are updating then only we can update
+
     def create
         @user = User.new(user_params)
         if @user.save
