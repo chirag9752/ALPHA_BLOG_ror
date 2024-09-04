984ae8e739e277eafb67c7b6847717f00f6106ab
Chirag Agrawal <you@example.com>
2024-09-05 00:24:00 +0530

    admin user requirement and test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "Johndoe", email: "johndoe@gmail.com",
+     password: "password", admin: true)
   end
 
   test "should get index" do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
@@ -18,11 +21,20 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   # even through we are creating travel category over here but when run category.all in rails console it's still empty because its test db not a real db
   test "should create category" do
+    sign_in_as(@admin_user)
     assert_difference("Category.count", 1) do
       post categories_url, params: { category: { name: 'Travel' } }
     end
 
     assert_redirected_to category_url(Category.last)
+  end
+
+  test "should not create category if not admin" do
+    assert_no_difference("Category.count") do
+      post categories_url, params: { category: { name: 'Travel' } }
+    end
+
+    assert_redirected_to categories_url
   end
 
   test "should show category" do
--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-      @category = Category.create(name: "Sports")
-      @category2 = Category.create(name: "Travel")
+    @category = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
   end
 
   test "should show categories listing" do
